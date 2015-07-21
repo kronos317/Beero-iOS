7567b0080491fde2adc3503463433d33db4ab1dd
Chris Lin <chrislin.317@hotmail.com>
2015-07-21 16:33:46 +0800

    Initial Commit

---
+++ Beero/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Beero
+//
+//  Created by Chris Lin on 7/21/15.
+//  Copyright (c) 2015 Beero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Beero/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Beero
+//
+//  Created by Chris Lin on 7/21/15.
+//  Copyright (c) 2015 Beero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
